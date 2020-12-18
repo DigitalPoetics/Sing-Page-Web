b1582da3bacdf77775212c0f8ae2c2f3bcef1732
diagrammaticreadings <diagrammaticreadings@hotmail.com>
2020-12-18 01:48:08 -0500

    getting there!

--- js.js
+++ js.js
@@ -1,1 +1,25 @@
-var x = "Hello World!"
+'use strict';
+
+var wavesurfer;
+
+// Init & load
+document.addEventListener('DOMContentLoaded', function() {
+    var pluginOptions = {
+        waveColor: 'grey',
+        progressColor: 'black',
+        height: 30
+    };
+    var options = {
+        container: '#waveform',
+        waveColor: 'violet',
+        progressColor: 'purple',
+        loaderColor: 'purple',
+        cursorColor: 'navy',
+        plugins: [WaveSurfer.minimap.create(pluginOptions)]
+    };
+
+    // Init wavesurfer
+    wavesurfer = WaveSurfer.create(options);
+
+    wavesurfer.load('../example/media/demo.wav');
+});
